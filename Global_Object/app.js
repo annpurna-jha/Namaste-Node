8cf5588493e37d64e33dcb943ac8259fde3f2465
annpurna-jha <annpurnajha4@gmail.com>
2024-09-18 14:46:09 +0530

    global object in node

---
+++ Global_Object/app.js
@@ -0,0 +1,9 @@
+var name = "namste node";
+var a = 10;
+var b = 20;
+console.log(name);
+console.log(a+b);
+
+console.log(global); //global object in node js
+console.log(this); //this will give an empty object,not a global object like in browser
+console.log(globalThis === global); //globalThis is a global object which is a standard way to represent global, window, tis, self,frames etc
