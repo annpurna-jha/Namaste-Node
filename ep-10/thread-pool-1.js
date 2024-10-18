aace9ba26808514baab331ce896764a08237c4f0
annpurna-jha <annpurnajha4@gmail.com>
2024-10-18 08:58:22 +0530

    working of thread pool

--- ep-10/thread-pool-1.js
+++ ep-10/thread-pool-1.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const crypto = require("crypto");
 
 // order of execution is not guaranteed
+// by default size of thread pool is 4
+
 crypto.pbkdf2("password","salt",5000000,50,"sha512",(err,key) =>{
     console.log("1 - cryptoPBKDF2 done")
 });
