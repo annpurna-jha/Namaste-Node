961908d8b9a92abee8bae0081da08d139101b0d5
annpurna-jha <annpurnajha4@gmail.com>
2024-10-23 19:55:34 +0530

    Modify

--- Global_Object/app.js
+++ Global_Object/app.js
@@ -6,4 +6,4 @@ console.log(a+b);
 
 console.log(global); //global object in node js
 console.log(this); //this will give an empty object,not a global object like in browser
-console.log(globalThis === global); //globalThis is a global object which is a standard way to represent global, window, tis, self,frames etc
+console.log(globalThis === global); //globalThis is a global object which is a standard way to represent global, window, this, self,frames etc
