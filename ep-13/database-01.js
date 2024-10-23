3968ebae47d61a92c4b421a0b0b3e8a2619b01fb
annpurna-jha <annpurnajha4@gmail.com>
2024-10-23 19:36:47 +0530

    Basics of mongo db

---
+++ ep-13/database-01.js
@@ -0,0 +1,15 @@
+const URI = 
+"mongodb+srv://annpurnajha4:Annpurna2002@namastenode.t4jex.mongodb.net/"; // connection string
+
+// Notes
+
+// go to mongo db website
+// Create a free M0 cluster
+// create a user
+// get the connection string
+// install mongo db compass
+// create a cluster in which you can have multiple db we created NamasteNodeJs
+// create a db (created HelloWorld)
+// create collection (created user; collection means tables)
+// insert documents into collection(i.e rows)
+// you can add documents via compass interface or with code
