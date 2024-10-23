4b8992d5bf9fe719daf133207733cd6a05d98e24
annpurna-jha <annpurnajha4@gmail.com>
2024-10-23 19:52:56 +0530

    ES modules

---
+++ ES_Modules/index.js
@@ -0,0 +1,5 @@
+import {x,multiply} from './operation.js';
+
+console.log("hello from index.js");
+console.log(x);
+console.log(multiply(2,3));
---
+++ ES_Modules/operation.js
@@ -0,0 +1,7 @@
+const add = (a,b) => {
+    return a+b;
+} 
+export function multiply (a,b){
+    return a*b;
+}
+export const x = "hello world"
